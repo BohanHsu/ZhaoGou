e2f82071351d8bc60dba3ce2dd17f31a5de7e3f7
Bohan Xu <ilovebarce@gmail.com>
2021-08-20 23:31:35 -0400

    copied code work

--- app/src/main/java/com/example/zhaogou/MainActivity.kt
+++ app/src/main/java/com/example/zhaogou/MainActivity.kt
@@ -1,11 +1,197 @@
 package com.example.zhaogou
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import io.fotoapparat.Fotoapparat
+import io.fotoapparat.configuration.CameraConfiguration
+import io.fotoapparat.log.logcat
+import io.fotoapparat.log.loggers
+import io.fotoapparat.parameter.ScaleType
+import io.fotoapparat.selector.back
+import io.fotoapparat.selector.front
+import io.fotoapparat.selector.off
+import io.fotoapparat.selector.torch
+import io.fotoapparat.view.CameraView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    private val dirName = "mydir"
+
+
+    private val permissions = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    private var fotoapparat: Fotoapparat? = null
+
+    private var fotoapparatState : FotoapparatState? = null
+    private var cameraStatus : CameraState? = null
+    private var flashState: FlashState? = null
+
+    private val filename = "test.jpg"
+
+    private val storageDir = Environment.getDataDirectory()
+    private val dest = File(storageDir, filename)
+
+    fun generateFileForPhoto(): File {
+        val fileName = "photo" + (System.currentTimeMillis() / 1000L).toString() + ".jpg"
+        val dir: File = File(this.getFilesDir(), dirName)
+        if (!dir.exists()) {
+            dir.mkdir()
+        }
+        return File(dir, fileName)
+    }
+
+    fun listAllPhotos() : List<String> {
+        val dir: File = File(this.getFilesDir(), dirName)
+        val list = mutableListOf<String>();
+        dir.listFiles().forEach {
+            list.add(it.absolutePath)
+        }
+        return list
+    }
+
+    // Android life cycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createFotoapparat()
+
+        cameraStatus = CameraState.BACK
+        flashState = FlashState.OFF
+        fotoapparatState = FotoapparatState.OFF
+
+        // Button handlers
+        fab_camera.setOnClickListener {
+            takePhoto()
+        }
+
+        fab_switch_camera.setOnClickListener {
+            switchCamera()
+        }
+
+        fab_flash.setOnClickListener {
+            changeFlashState()
+        }
     }
+
+    override fun onStop() {
+        super.onStop()
+        fotoapparat?.stop()
+        fotoapparatState = FotoapparatState.OFF;
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (hasNoPermissions()) {
+            requestPermission()
+        }else{
+            fotoapparat?.start()
+            fotoapparatState = FotoapparatState.ON
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(!hasNoPermissions() && fotoapparatState == FotoapparatState.OFF){
+            val intent = Intent(baseContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    // foto
+    private fun createFotoapparat(){
+        val cameraView = findViewById<CameraView>(R.id.camera_view)
+
+        fotoapparat = Fotoapparat(
+            context = this,
+            view = cameraView,
+            scaleType = ScaleType.CenterCrop,
+            lensPosition = back(),
+            logger = loggers(
+                logcat()
+            ),
+            cameraErrorCallback = { error ->
+                println("Recorder errors: $error")
+            }
+        )
+    }
+
+    // button handler
+    private fun takePhoto() {
+        if (hasNoPermissions()) {
+            requestPermission()
+        } else {
+            val result = fotoapparat
+                ?.takePicture()
+                ?.saveToFile(generateFileForPhoto())
+
+
+            listAllPhotos().forEach() {
+                print("xbh1 photo nane: " + it)
+            }
+//                ?.saveToFile(dest)
+
+//            writeFileOnInternalStorage(this, "testfilename.txt", "helloworld")
+//            generateFileForPhoto
+//            print("xbh1:" + result.toString())
+        }
+    }
+
+    private fun switchCamera() {
+        fotoapparat?.switchTo(
+            lensPosition =  if (cameraStatus == CameraState.BACK) front() else back(),
+            cameraConfiguration = CameraConfiguration()
+        )
+
+        if(cameraStatus == CameraState.BACK) cameraStatus = CameraState.FRONT
+        else cameraStatus = CameraState.BACK
+    }
+
+    private fun changeFlashState() {
+        fotoapparat?.updateConfiguration(
+            CameraConfiguration(
+                flashMode = if(flashState == FlashState.TORCH) off() else torch()
+            )
+        )
+
+        if(flashState == FlashState.TORCH) flashState = FlashState.OFF
+        else flashState = FlashState.TORCH
+    }
+
+    // permission
+    private fun hasNoPermissions(): Boolean{
+        return ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this,
+            Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+    }
+
+    fun requestPermission(){
+        ActivityCompat.requestPermissions(this, permissions, 0)
+    }
+}
+
+enum class CameraState{
+    FRONT, BACK
+}
+
+enum class FlashState{
+    TORCH, OFF
+}
+
+enum class FotoapparatState{
+    ON, OFF
 }
